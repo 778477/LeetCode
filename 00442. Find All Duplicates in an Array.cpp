aff9e352e043e1bab4293ac69af70dff17a99af3
778477 <1032419360@qq.com>
2017-02-09 21:36:34 +0800

    update 442

--- 00442. Find All Duplicates in an Array.cpp
+++ 00442. Find All Duplicates in an Array.cpp
@@ -1,26 +1,18 @@
 //
 //  main.cpp
-//  leetCode
+//  LeetCode
 //
-//  Created by miaoyou.gmy
-//  Copyright © 2016年 miaoyou.gmy. All rights reserved.
+//  Created by 郭妙友 on 17/2/5.
+//  Copyright © 2017年 miaoyou.gmy. All rights reserved.
 //
 
 #include <iostream>
 #include <vector>
 #include <string>
-#include <queue>
-#include <stack>
-#include <list>
+#include <cmath>
 #include <map>
-#include <set>
-#include <algorithm>
-#include <functional>
-#include "math.h"
-#include "stdlib.h"
-#include "stdio.h"
-using namespace std;
 
+using namespace std;
 
 /**
  
@@ -41,69 +33,49 @@ using namespace std;
  
  */
 
+
 /**
  
  28 / 28 test cases passed.
  Status: Accepted
- Runtime: 162 ms
+ Runtime: 209 ms
  
  */
 
 class Solution {
 public:
-    vector<int> findDouble(vector<int> nums) {
+    vector<int> findDuplicates(vector<int>& nums) {
         vector<int> ans;
         
-        for(int i=0;i<nums.size();i++){
-            int tmp = nums[i]-1,a;
-            
-            if(nums[i] > 0 && nums[tmp] <= 0){
-                nums[tmp]--;
-                nums[i] = 0;
-                continue;
+        int index = 0;
+        while(index < nums.size()){
+            if(nums[index] != nums[nums[index] - 1]){
+                swap(nums[index], nums[nums[index] - 1]);
+            } else{
+                index++;
             }
-            
-            while(tmp > -1 && nums[tmp] >= 0){
-                a = nums[tmp] - 1;
-                nums[tmp] > 0 ? nums[tmp] = -1 : --nums[tmp];
-                
-                if(a < 0) break;
-                tmp = a;
-                if(nums[tmp] <= 0) --nums[tmp];
-                nums[i] = 0;
-            }
-            
         }
         
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] < -1){
-                ans.push_back(i+1);
+        for(index = 0; index < nums.size(); index++){
+            if(nums[index] != index+1){
+                ans.push_back(nums[index]);
             }
         }
+        
         
         return ans;
     }
 };
 
-/**
- 
- test case 1:
- 3,3,5,6,6,7,1,2
- 
- test case 2:
- 4,3,2,7,8,2,3,1
- 
- test case 3:
- 2,2
- */
-
 int main(){
-    vector<int> nums({1,2,3,3,5,6,6,7});
     Solution solve;
+    vector<int> nums({4,3,2,7,8,2,3,1});
+    vector<int> ans = solve.findDuplicates(nums);
     
-    vector<int> ans = solve.findDouble(nums);
     
     for_each(ans.begin(), ans.end(), [](int num){
         cout<<num<<endl;
     });
+    
+    return 0;
 }
