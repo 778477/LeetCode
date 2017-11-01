2bd294cbdfc10c9b6bb6e27367b7986970398ce8
778477 <1032419360@qq.com>
2017-11-01 17:24:41 +0800

    update 347

--- 00347. Top k Frequent Elements.cpp
+++ 00347. Top k Frequent Elements.cpp
@@ -1,33 +1,17 @@
 //
 //  main.cpp
-//  leetCode
+//  leetcode-whiteboard
 //
 //  Created by miaoyou.gmy
-//  Copyright © 2016年 miaoyou.gmy. All rights reserved.
+//  Copyright © 2017年 miaoyou.gmy. All rights reserved.
 //
 
-#include <iostream>
-#include <vector>
-#include <string>
-#include <queue>
-#include <stack>
-#include <list>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <functional>
-#include "math.h"
-#include "stdlib.h"
-#include "stdio.h"
-using namespace std;
 
-/*
+
+/**
  
  347. Top K Frequent Elements
- Total Accepted: 41203
- Total Submissions: 90710
- Difficulty: Medium
- Contributors: Admin
+ 
  Given a non-empty array of integers, return the k most frequent elements.
  
  For example,
@@ -36,87 +20,62 @@ using namespace std;
  Note:
  You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
  Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
- Subscribe to see which companies asked this question
+
+ 
  */
 
-
-
-/*
+/**
  
  20 / 20 test cases passed.
  Status: Accepted
- Runtime: 422 ms
+ Runtime: 19 ms
  
  */
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <queue>
+#include <unordered_map>
 
+using namespace std;
 
 class Solution {
 public:
-    Solution(){
-        _maxFrequent = 0;
-    }
-    
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<int> ans;
         
-        for_each(nums.begin(), nums.end(), [&](const int num){
-            
-            if(table.count(num)){
-                int oldF = table[num];
-                frequentTable[oldF].erase(iterTable[num]);
-                iterTable.erase(num);
-            }
-            
-            ++table[num];
-            _maxFrequent = _maxFrequent > table[num] ? _maxFrequent : table[num]; // update maxFrequent
-            frequentTable[table[num]].push_back(num);
-            iterTable[num] = --frequentTable[table[num]].end();
-            
-        });
+        unordered_map<int, int> cnt;
+        for(int num : nums){
+            ++cnt[num];
+        }
+
+        priority_queue<pair<int, int>> q;
+        for(auto i : cnt){
+            q.push(make_pair(i.second, i.first));
+        }
         
-        while(k > 0){
-            vector<int> tmp;
-            copy(frequentTable[_maxFrequent].begin(), frequentTable[_maxFrequent].end(), back_inserter(tmp));
-            
-            //            sort(tmp.begin(), tmp.end(), less<int>());
-            
-            if(tmp.size() <= k){
-                k -= tmp.size();
-                for_each(tmp.begin(), tmp.end(), [&](const int num){
-                    ans.push_back(num);
-                });
-                
-            } else{
-                for(int i=0;i<k;i++){
-                    ans.push_back(tmp[i]);
-                }
-                k = 0;
-            }
-            
-            --_maxFrequent;
+        while(k--){
+            ans.push_back(q.top().second);
+            q.pop();
         }
         
         return ans;
     }
-private:
-    int _maxFrequent;
-    map<int,int> table;
-    map<int, list<int>> frequentTable;
-    map<int, list<int>::iterator> iterTable;
 };
 
 
-int main(){
-    Solution solve;
-    vector<int> nums({5,3,1,1,1,3,73,1});
+int main(int argc, const char * argv[]) {
     
+    Solution solve;
+    
+    
+    vector<int> nums = {1,1,1,2,2,3};
     vector<int> ans = solve.topKFrequent(nums, 1);
     
-    for_each(ans.begin(), ans.end(), [](const int num){
-        cout<<num<<endl;
-    });
-    
+    for(int i : ans){
+        cout<<i<<endl;
+    }
     
     return 0;
 }
-
